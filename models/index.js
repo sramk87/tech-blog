d18f67bcc734465e83bb41596c279e8965840a88
sramk87 <sramkissoon87@gmail.com>
2022-11-30 01:07:11 -0500

    models updated

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+}); 
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
