1448ebf607283b249c906e5080a3959216a1d445
sramk87 <sramkissoon87@gmail.com>
2022-11-30 00:29:57 -0500

    user routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,13 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
         'id',
-        'post_text',
-        'title'
+        'title',
+        'post_text'
     ],
     include: [
       {
@@ -33,3 +33,57 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get('/homepage', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return; 
+    }
+    res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'post_text'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post available, try again.' });
+          return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn});
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const routes = require('./controllers/');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
